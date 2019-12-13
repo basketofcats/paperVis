5200807b33d2296a5bd8e75b2f2227a274ee71db
jiahy <632239375@qq.com>
2019-12-13 18:00:31 +0800

    dynamic wordle(year range)

--- data/wordle-data-preprocess.py
+++ data/wordle-data-preprocess.py
@@ -31,32 +31,51 @@ def word_filter(key_cnt):
         except Exception:
             pass
 
-if __name__ == '__main__':
-    dir_name = sorted(os.listdir('./organized'))
-    for name in dir_name:
-        if name.endswith('.json'):
-            parse_dir(name)
-            print("process finish : ", name)
-            # break
-
-    # parse_dir('iccv2015_paper_infos.json')
-
+def parse_file(filename):
+    parse_dir(filename)
+    print("process finish : ", filename)
     count = Counter(keywords_cnt)
-    rank = count.most_common()[:100]
+    rank = count.most_common()[:50]
 
     info = []
     m = {}
-
     for items in rank:
-        # m = []
-        # m.append(items[0])
-        # m.append(items[1])
         m.clear()
         m["text"] = items[0]
         m["size"] = items[1]
         info.append(m.copy())
 
-    json.dump(info, open('wordle-info.json', "w"));
+    to_file = "wordle-info-" + filename[4: 8] + ".json"
+    json.dump(info, open(to_file, "w"));
+
+if __name__ == '__main__':
+    dir_name = sorted(os.listdir('./organized'))
+    for name in dir_name:
+        if name.endswith('.json'):
+            keywords_cnt.clear()
+            parse_file(name)
+            # parse_dir(name)
+            # print("process finish : ", name)
+            # break
+
+    # parse_dir('iccv2015_paper_infos.json')
+
+    # count = Counter(keywords_cnt)
+    # rank = count.most_common()[:50]
+    #
+    # info = []
+    # m = {}
+    #
+    # for items in rank:
+    #     # m = []
+    #     # m.append(items[0])
+    #     # m.append(items[1])
+    #     m.clear()
+    #     m["text"] = items[0]
+    #     m["size"] = items[1]
+    #     info.append(m.copy())
+    #
+    # json.dump(info, open('wordle-info.json', "w"))
 
     # # author 有6471人
     # print(len(info['rela_a2p'].keys()))
